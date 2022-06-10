0096d1b1fb69bcf7b8a6b5e58f36b8cae91978b0
AlbertoDoc <alberto-oliveira-santos@hotmail.com>
2022-06-10 12:28:03 -0300

    feature(LoginActivity): Adding fields for login.

--- app/src/main/java/com/bestoffers/login/LoginActivity.kt
+++ app/src/main/java/com/bestoffers/login/LoginActivity.kt
@@ -1,6 +1,188 @@
 package com.bestoffers.login
 
+import android.os.Bundle
 import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.viewModels
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Visibility
+import androidx.compose.material.icons.filled.VisibilityOff
+import androidx.compose.material.icons.rounded.Email
+import androidx.compose.runtime.*
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.FocusDirection
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalFocusManager
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.input.VisualTransformation
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.bestoffers.ui.theme.BestOffersTheme
 
 class LoginActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        val model: LoginViewModel by viewModels()
+
+        setContent {
+            BestOffersTheme {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
+                ) {
+                    Scaffold {
+                        LoginForm(model)
+                    }
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun LoginForm(model: LoginViewModel) {
+    val focusManager = LocalFocusManager.current
+    var isPasswordVisible by remember { mutableStateOf(false) }
+
+    Column {
+        AppTextField(
+            text = model.email,
+            placeholder = "Email",
+            onChange = {
+                model.email = it
+            },
+            imeAction = ImeAction.Next,
+            keyboardType = KeyboardType.Text,
+            keyBoardActions = KeyboardActions(
+                onNext = {
+                    focusManager.moveFocus(FocusDirection.Down)
+                }
+            )
+        )
+
+        PasswordTextField(
+            text = model.password,
+            placeholder = "Senha",
+            onChange = {
+                model.password = it
+            },
+            imeAction = ImeAction.Next,
+            keyboardType = KeyboardType.Password,
+            keyBoardActions = KeyboardActions(
+                onNext = {
+                    focusManager.clearFocus(true)
+                }
+            ),
+            leadingIcon = {
+                IconButton(onClick = {
+                    isPasswordVisible = !isPasswordVisible
+                }) {
+                    Icon(
+                        imageVector = if (isPasswordVisible)
+                            Icons.Filled.Visibility
+                        else
+                            Icons.Filled.VisibilityOff,
+                        contentDescription = "Password Visibility"
+                    )
+                }
+            },
+            visualTransformation = if (isPasswordVisible) {
+                VisualTransformation.None
+            } else {
+                PasswordVisualTransformation()
+            }
+        )
+        ConfirmButton({})
+    }
+}
+
+@Composable
+fun ConfirmButton(onClick: () -> Unit) {
+    Button(
+        onClick = onClick,
+        contentPadding = PaddingValues(
+            start = 20.dp,
+            top = 12.dp,
+            end = 20.dp,
+            bottom = 12.dp
+        )) {
+        Text("Entrar")
+    }
+}
+
+@Composable
+fun AppTextField(
+    modifier: Modifier = Modifier,
+    text: String,
+    placeholder: String,
+    leadingIcon: @Composable (() -> Unit)? = null,
+    onChange: (String) -> Unit = {},
+    imeAction: ImeAction = ImeAction.Next,
+    keyboardType: KeyboardType = KeyboardType.Text,
+    keyBoardActions: KeyboardActions = KeyboardActions(),
+    isEnabled: Boolean = true,
+) {
+    OutlinedTextField(
+        modifier = modifier.fillMaxWidth(),
+        value = text,
+        onValueChange = onChange,
+        leadingIcon = leadingIcon,
+        textStyle = TextStyle(fontSize = 18.sp),
+        keyboardOptions = KeyboardOptions(imeAction = imeAction, keyboardType = keyboardType),
+        keyboardActions = keyBoardActions,
+        enabled = isEnabled,
+        colors = TextFieldDefaults.outlinedTextFieldColors(
+            focusedBorderColor = Color.Black,
+            unfocusedBorderColor = Color.Gray,
+            disabledBorderColor = Color.Gray,
+            disabledTextColor = Color.Black
+        ),
+        placeholder = {
+            Text(text = placeholder, style = TextStyle(fontSize = 18.sp, color = Color.LightGray))
+        }
+    )
+}
+
+
+@Composable
+fun PasswordTextField(
+    modifier: Modifier = Modifier,
+    text: String,
+    placeholder: String,
+    leadingIcon: @Composable (() -> Unit)? = null,
+    onChange: (String) -> Unit = {},
+    imeAction: ImeAction = ImeAction.Next,
+    keyboardType: KeyboardType = KeyboardType.Text,
+    keyBoardActions: KeyboardActions = KeyboardActions(),
+    isEnabled: Boolean = true,
+    visualTransformation: VisualTransformation
+) {
+    OutlinedTextField(
+        modifier = modifier.fillMaxWidth(),
+        value = text,
+        onValueChange = onChange,
+        leadingIcon = leadingIcon,
+        textStyle = TextStyle(fontSize = 18.sp),
+        keyboardOptions = KeyboardOptions(imeAction = imeAction, keyboardType = keyboardType),
+        keyboardActions = keyBoardActions,
+        enabled = isEnabled,
+        colors = TextFieldDefaults.outlinedTextFieldColors(
+            focusedBorderColor = Color.Black,
+            unfocusedBorderColor = Color.Gray,
+            disabledBorderColor = Color.Gray,
+            disabledTextColor = Color.Black
+        ),
+        placeholder = {
+            Text(text = placeholder, style = TextStyle(fontSize = 18.sp, color = Color.LightGray))
+        },
+        visualTransformation = visualTransformation
+    )
 }
--- app/src/main/java/com/bestoffers/main/MainActivity.kt
+++ app/src/main/java/com/bestoffers/main/MainActivity.kt
@@ -1,21 +1,32 @@
 package com.bestoffers.main
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.viewModels
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.Button
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material.icons.rounded.ArrowForward
+import androidx.compose.material.icons.rounded.Login
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.bestoffers.login.LoginActivity
 import com.bestoffers.ui.theme.BestOffersTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        val model: MainActivityViewModel by viewModels()
+
         setContent {
             BestOffersTheme {
                 // A surface container using the 'background' color from the theme
@@ -23,22 +34,45 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    Button()
+                    Scaffold {
+                        LoginButton { startLoginActivity() }
+                    }
                 }
             }
         }
     }
+
+    private fun startLoginActivity() {
+        val loginIntent = Intent(this, LoginActivity::class.java)
+        startActivity(loginIntent)
+    }
 }
 
 @Composable
-fun LoginButton() {
-    Button(text = "Hello $name!")
+fun LoginButton(onClick: () -> Unit) {
+    Button(
+        onClick = onClick,
+        contentPadding = PaddingValues(
+            start = 20.dp,
+            top = 12.dp,
+            end = 20.dp,
+            bottom = 12.dp
+        )
+    ) {
+        Icon(
+            Icons.Rounded.Login,
+            contentDescription = "Login",
+            modifier = Modifier.size(ButtonDefaults.IconSize)
+        )
+        Spacer(Modifier.size(ButtonDefaults.IconSpacing))
+        Text("Login")
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     BestOffersTheme {
-        Greeting("Android")
+        LoginButton(onClick = {})
     }
 }
