179af78362d7cc72dca04307b92a386dde70679b
AlbertoDoc <alberto-oliveira-santos@hotmail.com>
2022-06-11 11:46:27 -0300

    feat(RegisterActivity): Adding fields for new register.

--- app/src/main/java/com/bestoffers/register/RegisterActivity.kt
+++ app/src/main/java/com/bestoffers/register/RegisterActivity.kt
@@ -4,12 +4,30 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.viewModels
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Scaffold
-import androidx.compose.material.Surface
-import androidx.compose.runtime.Composable
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Visibility
+import androidx.compose.material.icons.filled.VisibilityOff
+import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.FocusDirection
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalFocusManager
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.input.VisualTransformation
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.bestoffers.login.AppTextField
+import com.bestoffers.login.PasswordTextField
 import com.bestoffers.ui.theme.BestOffersTheme
 
 class RegisterActivity : ComponentActivity() {
@@ -35,5 +53,174 @@ class RegisterActivity : ComponentActivity() {
 
 @Composable
 fun RegisterForm(model: RegisterViewModel) {
+    val focusManager = LocalFocusManager.current
+    var isPasswordVisible by remember { mutableStateOf(false) }
 
+    Column {
+        AppTextField(
+            text = model.firstName,
+            placeholder = "Primeiro Nome",
+            onChange = {
+                model.firstName = it
+            },
+            imeAction = ImeAction.Next,
+            keyboardType = KeyboardType.Text,
+            keyBoardActions = KeyboardActions(
+                onNext = {
+                    focusManager.moveFocus(FocusDirection.Down)
+                }
+            )
+        )
+
+        AppTextField(
+            text = model.lastName,
+            placeholder = "Sobrenome",
+            onChange = {
+                model.lastName = it
+            },
+            imeAction = ImeAction.Next,
+            keyboardType = KeyboardType.Text,
+            keyBoardActions = KeyboardActions(
+                onNext = {
+                    focusManager.moveFocus(FocusDirection.Down)
+                }
+            )
+        )
+
+        AppTextField(
+            text = model.email,
+            placeholder = "Email",
+            onChange = {
+                model.email = it
+            },
+            imeAction = ImeAction.Next,
+            keyboardType = KeyboardType.Text,
+            keyBoardActions = KeyboardActions(
+                onNext = {
+                    focusManager.moveFocus(FocusDirection.Down)
+                }
+            )
+        )
+
+        PasswordTextField(
+            text = model.password,
+            placeholder = "Senha",
+            onChange = {
+                model.password = it
+            },
+            imeAction = ImeAction.Next,
+            keyboardType = KeyboardType.Password,
+            keyBoardActions = KeyboardActions(
+                onNext = {
+                    focusManager.clearFocus(true)
+                }
+            ),
+            leadingIcon = {
+                IconButton(onClick = {
+                    isPasswordVisible = !isPasswordVisible
+                }) {
+                    Icon(
+                        imageVector = if (isPasswordVisible)
+                            Icons.Filled.Visibility
+                        else
+                            Icons.Filled.VisibilityOff,
+                        contentDescription = "Password Visibility"
+                    )
+                }
+            },
+            visualTransformation = if (isPasswordVisible) {
+                VisualTransformation.None
+            } else {
+                PasswordVisualTransformation()
+            }
+        )
+
+        ConfirmButton {
+
+        }
+    }
+}
+
+@Composable
+fun AppTextField(
+    modifier: Modifier = Modifier,
+    text: String,
+    placeholder: String,
+    leadingIcon: @Composable (() -> Unit)? = null,
+    onChange: (String) -> Unit = {},
+    imeAction: ImeAction = ImeAction.Next,
+    keyboardType: KeyboardType = KeyboardType.Text,
+    keyBoardActions: KeyboardActions = KeyboardActions(),
+    isEnabled: Boolean = true,
+) {
+    OutlinedTextField(
+        modifier = modifier.fillMaxWidth(),
+        value = text,
+        onValueChange = onChange,
+        leadingIcon = leadingIcon,
+        textStyle = TextStyle(fontSize = 18.sp),
+        keyboardOptions = KeyboardOptions(imeAction = imeAction, keyboardType = keyboardType),
+        keyboardActions = keyBoardActions,
+        enabled = isEnabled,
+        colors = TextFieldDefaults.outlinedTextFieldColors(
+            focusedBorderColor = Color.Black,
+            unfocusedBorderColor = Color.Gray,
+            disabledBorderColor = Color.Gray,
+            disabledTextColor = Color.Black
+        ),
+        placeholder = {
+            Text(text = placeholder, style = TextStyle(fontSize = 18.sp, color = Color.LightGray))
+        }
+    )
+}
+
+
+@Composable
+fun PasswordTextField(
+    modifier: Modifier = Modifier,
+    text: String,
+    placeholder: String,
+    leadingIcon: @Composable (() -> Unit)? = null,
+    onChange: (String) -> Unit = {},
+    imeAction: ImeAction = ImeAction.Next,
+    keyboardType: KeyboardType = KeyboardType.Text,
+    keyBoardActions: KeyboardActions = KeyboardActions(),
+    isEnabled: Boolean = true,
+    visualTransformation: VisualTransformation
+) {
+    OutlinedTextField(
+        modifier = modifier.fillMaxWidth(),
+        value = text,
+        onValueChange = onChange,
+        leadingIcon = leadingIcon,
+        textStyle = TextStyle(fontSize = 18.sp),
+        keyboardOptions = KeyboardOptions(imeAction = imeAction, keyboardType = keyboardType),
+        keyboardActions = keyBoardActions,
+        enabled = isEnabled,
+        colors = TextFieldDefaults.outlinedTextFieldColors(
+            focusedBorderColor = Color.Black,
+            unfocusedBorderColor = Color.Gray,
+            disabledBorderColor = Color.Gray,
+            disabledTextColor = Color.Black
+        ),
+        placeholder = {
+            Text(text = placeholder, style = TextStyle(fontSize = 18.sp, color = Color.LightGray))
+        },
+        visualTransformation = visualTransformation
+    )
+}
+
+@Composable
+fun ConfirmButton(onClick: () -> Unit) {
+    Button(
+        onClick = onClick,
+        contentPadding = PaddingValues(
+            start = 20.dp,
+            top = 12.dp,
+            end = 20.dp,
+            bottom = 12.dp
+        )
+    ) {
+        Text("Registrar")
+    }
 }
